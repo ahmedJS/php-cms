a5905ba6253173c71b7520df50efd4ed60512337
AhmedJS <ahmed_alsadi56@outlook.com>
2022-06-06 00:02:56 +0300

    make logic of convert jsonsetting into php class

--- app/src/RunCommand.php
+++ app/src/RunCommand.php
@@ -10,7 +10,41 @@ class RunCommand extends Command{
 
     function perform()
     {
-        $file = file_get_contents("../../cms-app-config.json");
+        $file = file_get_contents(__dir__."/../../cms-app-config.json");
+
+        $this->json2class($file);
+     
+    }
+
+    /**
+     * @param string $file json text file
+     * @return String[] contains php classes
+     */
+    function json2class(string $file){
+
+        $classes = json_decode($file);
         
+        $processedClasses = [];
+        
+        foreach($classes as $class => $properties) 
+        {
+            $propertiesMeta = "";
+
+            foreach($properties as $property => $value)
+            {
+                $propertiesMeta .= 'public '.$property.' = "'.$value.'"; ';
+            }
+
+            
+            $class_meta =  '
+             <?php
+                namespace CMSApp\generatedClasses;
+                class '.$class.' {
+                    '.$propertiesMeta.'
+                }
+            ';
+            array_push($processedClasses,$class_meta);
+        }
+        return $processedClasses;
     }
 }
--- tests/AppTest.php
+++ tests/AppTest.php
@@ -1,8 +1,10 @@
 <?php
 use PHPUnit\Framework\TestCase;
+use CMSApp\src\RunCommand;
 
 class AppTest extends TestCase{
     function testSomething(){
-        
+        $command = new RunCommand();
+        $command->perform();
     }
 }
