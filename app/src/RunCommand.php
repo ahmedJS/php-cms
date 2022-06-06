1db2f57920f96b2f1f7b360b8e76745802e50bdf
AhmedJS <ahmed_alsadi56@outlook.com>
2022-06-06 23:21:52 +0300

    generate class function and perform logic , write future wishes to the project

--- app/src/RunCommand.php
+++ app/src/RunCommand.php
@@ -1,24 +1,50 @@
 <?php
 namespace CMSApp\src;
 
+use Exception;
+use PhpParser\Node\Expr\Isset_;
 
+// make generate class function and perform logic
 class RunCommand extends Command{
-
+    /**
+     * @var String $default_webclass_distination
+     * specifying the default distination of the classes file
+     */
+    private $default_webclass_distination = __DIR__."/../website_classes/";
+    
     function __construct(private $options = array())
     {
+        if(!isset($options["config"]["website_class_distination"])){
+            $this->options["config"]["website_class_distination"]
+            = $this->default_webclass_distination;
+        };
     }
 
     function perform()
     {
+        // fetch the file
         $file = file_get_contents(__dir__."/../../cms-app-config.json");
 
-        $this->json2class($file);
-     
+        // get the classes
+        $classes = $this->json2class($file);
+        
+        foreach($classes as $class){
+            foreach($class as $className => $script_text){
+                $this->generateClass
+                (
+                    $script_text
+                    ,$this->options["config"]["website_class_distination"]
+                    .$className.".php"
+                );
+            }
+        }
     }
 
     /**
-     * @param string $file json text file
-     * @return String[] contains php classes
+     * 
+     * @param String $file json text file
+     * jsonFile Have to be of  " class " : {property : prop_value ... }
+     * @return Array[][] contains php classes ( name && script )
      */
     function json2class(string $file){
 
@@ -28,23 +54,52 @@ class RunCommand extends Command{
         
         foreach($classes as $class => $properties) 
         {
+            /**
+             * @var $propertiesMeta the actual line of the property in class
+             */
             $propertiesMeta = "";
+            
+            /**
+             * @var Array $class_meta contains class details
+             */
+            $class_meta = [];
 
             foreach($properties as $property => $value)
             {
-                $propertiesMeta .= 'public '.$property.' = "'.$value.'"; ';
+                $propertiesMeta .= 'public $'.$property.' = "'.$value.'"; ';
             }
 
-            
-            $class_meta =  '
-             <?php
-                namespace CMSApp\generatedClasses;
-                class '.$class.' {
-                    '.$propertiesMeta.'
-                }
-            ';
+            /**
+             * @var String $script the final script of the class
+             */
+
+            $script =  '
+            <?php
+               namespace CMSApp\generatedClasses;
+               class '.$class.' {
+                   '.$propertiesMeta.'
+               }
+           ';
+            $class_meta[$class] = $script;
+
             array_push($processedClasses,$class_meta);
         }
         return $processedClasses;
+    }
+
+    /**
+     * generate php script file (Class)
+     * @var string $script the script to store on
+     * @var string $full_file_destination the path that is file stored on
+     * @throws Exception when file is already exist
+     */
+    function generateClass($script , $full_file_destination ) {
+        
+        if(@file_get_contents($full_file_destination))
+        {
+            throw new Exception("one of the classes is already registered");
+        }
+
+        file_put_contents($full_file_destination ,$script );
     }
 }
